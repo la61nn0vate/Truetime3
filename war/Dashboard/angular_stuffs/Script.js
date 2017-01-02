6ce5d063523bc430f671c8d3ccf381ba08c36884
la61nn0vate <la61nn0vate@gmail.com>
2017-01-02 11:59:05 +0530

    Add site flow + other changes
    Submit

--- war/Dashboard/angular_stuffs/Script.js
+++ war/Dashboard/angular_stuffs/Script.js
@@ -974,25 +974,6 @@ app.controller("site_controller",['$scope','$timeout', function ($scope,$timeout
 	}
 
 
-	var shift_d=[];
-	$scope.add_shifts=function(){
-		var shift=[];
-
-		for(var i=0;i<$scope.number_of_shifts;i++)
-		{
-			shift[i]=[{ shift_name:"shift_name"+i,time_from:"time_from"+i,time_to:"time_to"+i}];
-
-		}
-		for(var i=0;i<$scope.u_number_of_shifts;i++)
-		{
-			shift[i]=[{ shift_name:"shift_name"+i,time_from:"time_from"+i,time_to:"time_to"+i}];
-
-		}
-		$scope.shifts=shift;
-		shift_d=shift;
-		console.log($scope.shifts);
-	}
-
 	$scope.addsite=function(){
 		gapi.client.organisationApi.getAdminOrganisation({'token': sessionStorage.accessToken}).execute(function (response) { //getting org_key from getAdminOrganisation() method of organisationApi
 			var org_key = response.websafeKey;
@@ -1030,7 +1011,7 @@ app.controller("site_controller",['$scope','$timeout', function ($scope,$timeout
 											if(!res.error)
 											{
 												swal({
-													  title: "Site Added Successfully",
+													  title: "Site & Shift Added Successfully",
 													  type: "success",
 													  timer: 3000,
 													  showConfirmButton: false
@@ -1050,7 +1031,7 @@ app.controller("site_controller",['$scope','$timeout', function ($scope,$timeout
 												{
 													var exception = res.message;
 													swal({
-														  title: "Site Not Added",
+														  title: "SIte & Shift Not Added",
 														  text: exception,
 														  type: "error",
 														  timer: 3000,
@@ -1072,6 +1053,77 @@ app.controller("site_controller",['$scope','$timeout', function ($scope,$timeout
 		});
 
 	};
+	
+	//select number of shifts
+	$('#number_of_shifts').change(function () {
+		$("#shift_details").addClass("show");
+		$("#shift_details").removeClass("hide");
+		var sel_value = $('#number_of_shifts option:selected').val();
+
+		console.log(sel_value);
+		if (sel_value == "Select Number Of Shifts") {
+			$("#shift_details").removeClass("show");
+			$("#shift_details").addClass("hide");
+			$("#shift_details").empty(); // Resetting Form
+			$("#shift_details").css({
+				'display': 'none'
+			});
+		} else {
+			$("#shift_details").empty(); //Resetting Form
+			// Below Function Creates Input Fields Dynamically
+			create(sel_value);
+
+		}
+	});
+	function create(sel_value) {
+		for (var i = 1; i <= sel_value; i++) {
+			$("div#shift_details").slideDown('slow');
+			$("div#shift_details").append($("#shift_details").append($("<div/>", {
+				id: 'head',
+				class: 'row'
+			}).append($("<h3/>").text("Shift " + i + " Details")), $("<div/>", { class: 'row form-group'
+
+
+			}).append($("<label/>", {
+				text: 'Shift Name',
+				class: 'control-label'
+
+			}), $("<input/>", {
+				type: 'text',
+				placeholder: 'Shift Name',
+				id: 'shift_name' + i,
+				required: "required",
+				class: 'form-control'
+			})), $("<br/>"), $("<div/>", { class: 'row form-group'
+
+
+			}).append($("<label/>", {
+				text: 'Time from',
+				class: 'control-label'
+
+			}), $("<input/>", {
+				type: 'time',
+				placeholder: 'Time From',
+				id: 'time_from' + i,
+				required: "required",
+				class: 'form-control'
+			})), $("<br/>"), $("<div/>", { class: 'row form-group'
+
+
+			}).append($("<label/>", {
+				text: 'Time to',
+				class: 'control-label'
+
+			}), $("<input/>", {
+				placeholder: 'Time To',
+				type: 'time',
+				id: 'time_to' + i,
+				required: "required",
+				class: 'form-control'
+			})), $("<hr/>")))
+		}
+	}
+	//!select shift
 
 	var site_key="";
 	$scope.manage_site=function(site)
@@ -1103,10 +1155,12 @@ app.controller("site_controller",['$scope','$timeout', function ($scope,$timeout
 			$scope.$apply(function () {
 				if(resp.error)
 				{
+					var exception = resp.message;
 					$scope.show_manage=false;
 					$scope.get_site_data();
 					swal({
 						  title: "Site Not Updated",
+						  text: exception,
 						  type: "error",
 						  timer: 3000,
 						  showConfirmButton: false
